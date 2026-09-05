c09db0497225285eb41eb5473eb766666650225b
13640226 <azjensekhak7@gmail.com>
2026-09-06 01:38:04 +0200

    feat(delivery): expose entitlement check to templates

---
+++ marketplace/templatetags/entitlements.py
@@ -0,0 +1,11 @@
+from django import template
+
+from apps.core.services import has_valid_entitlement
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def has_product_entitlement(user, product):
+    return has_valid_entitlement(user, product)
