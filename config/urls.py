93004d469e64cbec902a95125800265bec0f73e6
13640226 <azjensekhak7@gmail.com>
2026-09-11 02:03:46 +0200

    security: route metrics through machine guard

--- config/urls.py
+++ config/urls.py
@@ -12,12 +12,14 @@ from django.views.generic import RedirectView
 from apps.platform_shell.registry import registry as platform_registry
 from config.health import health_check
 from config.inspect_views import context_view, inspect_entry
+from config.metrics import metrics_view
 from config.views import home, profile, search_page
 
 
 # Non-localized operational and API endpoints.
 urlpatterns = [
     path("health/", health_check, name="health"),
+    path("metrics", metrics_view, name="prometheus-django-metrics"),
     path("", include("django_prometheus.urls")),
     path("i18n/", include("django.conf.urls.i18n")),
     path("api/", include("apps.core.urls")),
