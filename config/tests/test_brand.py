dc143a8c984968b7da87e63d14c32fd978ce9112
13640226 <azjensekhak7@gmail.com>
2026-09-12 06:23:13 +0200

    fix(brand): preserve theme-driven logo colors

--- config/tests/test_brand.py
+++ config/tests/test_brand.py
@@ -24,13 +24,21 @@ class BrandIntegrationTests(TestCase):
         self.assertIsNotNone(asset_path)
         self.assertTrue(Path(asset_path).is_file())
 
-    def test_navigation_css_uses_relative_mask_asset(self):
+    def test_navigation_css_uses_theme_driven_inline_mark_colors(self):
         css_path = finders.find("css/standalone-nav.css")
         self.assertIsNotNone(css_path)
         css = Path(css_path).read_text(encoding="utf-8")
 
-        self.assertIn('mask-image: url("../images/brand/ia-s.svg")', css)
-        self.assertIn('-webkit-mask-image: url("../images/brand/ia-s.svg")', css)
+        self.assertNotIn('mask-image: url("../images/brand/ia-s.svg")', css)
+        self.assertNotIn('-webkit-mask-image: url("../images/brand/ia-s.svg")', css)
+
+        trajectory_block = css.split(".vorneq-traj {", 1)[1].split("}", 1)[0]
+        cobalt_block = css.split(".vorneq-node--cobalt {", 1)[1].split("}", 1)[0]
+        discovery_block = css.split(".vorneq-node--cyan {", 1)[1].split("}", 1)[0]
+
+        self.assertIn("stroke: currentColor;", trajectory_block)
+        self.assertIn("fill: currentColor;", cobalt_block)
+        self.assertIn("fill: var(--color-accent);", discovery_block)
 
     def test_vorneq_theme_uses_brand_blue_without_changing_semantic_state_tokens(self):
         palette_path = finders.find("css/theme-palettes.css")
