744b614e4752e45a8fc0d79c04cf9d4b4b087d36
13640226 <azjensekhak7@gmail.com>
2026-09-22 05:36:07 +0200

    test: stabilize Discover graph locale assertion

--- apps/search/tests/test_pages.py
+++ apps/search/tests/test_pages.py
@@ -252,7 +252,7 @@ class DiscoverGraphV1AIntegrationTests(TestCase):
         artifact_filter.return_value.only.return_value.first.return_value = artifact
         graph.return_value = self._graph()
 
-        response = self.client.get(reverse("discover"), {"type": "libraryitem"})
+        with override("en"):\n            response = self.client.get(reverse("discover"), {"type": "libraryitem"})
 
         self.assertEqual(response.status_code, 200)
         artifact_filter.assert_called_once()
